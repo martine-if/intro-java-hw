567263664bcae57178a1823699aec72c4e280b00
Martine Ferrency <martine@ferrency.com>
2022-10-01 18:00:33 -0400

    Added battle data and outlined helper methods

--- Ferrency_HW2/src/ferrency/CLASSES/BattleData.java
+++ Ferrency_HW2/src/ferrency/CLASSES/BattleData.java
@@ -1,5 +1,15 @@
 package ferrency.CLASSES;
 
 public class BattleData {
-
+	public static String[][] data = { 
+			{"TP-1_SYNC","11","1","5","2"}, 
+			{"TP-2_SYTH","23","3","2","4"}, 
+			{"TP-3_SYBV","15","2","3","1"}, 
+			{"TP-4_SYDF","12","5","0","6"}, 
+			{"TP-9_TYTR","6","3","2","2"}, 
+			{"TP-8_TYUI","13","2","4","4"}, 
+			{"TP-7_TYPO","33","5","7","2"}, 
+			{"TP-6_TYEW","16","2","2","3"}, 
+			{"TP-5_UYHG","11","4","1","6"}, 
+			{"TP-0_UYIO","15","1","4","7"}};
 }
--- Ferrency_HW2/src/ferrency/CLASSES/ImperialAnalysis.java
+++ Ferrency_HW2/src/ferrency/CLASSES/ImperialAnalysis.java
@@ -1,5 +1,54 @@
 package ferrency.CLASSES;
 
 public class ImperialAnalysis {
+	public static double convertToDouble(String s) {
+		double d = 0;
+		//
+		return d;
+	}
 
+	public static double fighterKillPoints(double kills) {
+		double points = kills * 1.75;
+		return points;
+	}
+
+	public static double bomberKillPoints(double kills) {
+		double points = kills * 1.5;
+		return points;
+	}
+
+	public static double assaultFighterPoints(double kills) {
+		double points = kills * 1.25;
+		return points;
+	}
+
+	public static double missionsKillsRatio(int kills, int missions) {
+		double ratio = kills / missions;
+		return ratio;
+	}
+
+	public static String pilotValue(double mkRatio, double[] killPoints) {
+		String product = "";
+		//
+		return product;
+	}
+
+	public static String pilotRating(double pv) {
+		String rating = "";
+		// …greater than 20 Rating is 'Elite’
+		// …greater than 12 and less than or equal to 20 Rating is ‘Veteran’
+		// …less than or equal to 12 Rating is ‘Standard’
+		if (pv > 20) {
+			rating = "Elite";
+		} else if (pv > 12) {
+			rating = "Veteran";
+		} else {
+			rating = "Standard";
+		}
+		return rating;
+	}
+
+	public static void displayMissionData(String[][] data) {
+
+	}
 }
