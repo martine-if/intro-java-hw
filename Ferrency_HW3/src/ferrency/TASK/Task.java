3a713093ac0714cc7fc82fbbfae52e46f5d1ea19
Martine Ferrency <martine@ferrency.com>
2022-10-05 19:07:04 -0400

    Edits to processFile_B method

--- Ferrency_HW3/src/ferrency/TASK/Task.java
+++ Ferrency_HW3/src/ferrency/TASK/Task.java
@@ -1,15 +1,26 @@
 package ferrency.TASK;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Task {
-	public static String DIR = "C:\\Users\\marti\\git\\intro-java-hw\\Ferrency_HW3\\src\\ferrency\\DATA";
+	public static String DIR = "C:\\Users\\marti\\git\\intro-java-hw\\Ferrency_HW3\\src\\ferrency\\DATA\\";
 	
 	public static void processFile_B(String fn) throws IOException {
-		BufferedWriter BW = new BufferedWriter(new FileWriter(DIR + fn));
+		BufferedReader BR = new BufferedReader(new FileReader(DIR + fn));
 		
+		int count = (int)Files.lines(Paths.get(DIR + fn)).count();
+		String[] lines = new String[count];
 		
+		for(int i=0;i<count;i++) {
+			lines[i] = BR.readLine();
+		}
+		for(String i:lines) {
+			System.out.println(i);
+		}
+		BR.close();
 	}
 }
