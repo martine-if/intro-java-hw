994e1aaed4b84b32e6ffdac1b2329bc6a8990987
Martine Ferrency <martine@ferrency.com>
2022-10-31 15:57:25 -0400

    Created main method structure

--- Ferrency_HW4/src/ferrency/MAIN/Martine.java
+++ Ferrency_HW4/src/ferrency/MAIN/Martine.java
@@ -1,5 +1,19 @@
 package ferrency.MAIN;
 
 public class Martine {
-
+	public static void main(String[] args) {
+		//R3Factory = new R3Factory("R3FAC");
+		//R4Factory = new R4Factory("R4FAC");
+		
+		System.out.println();
+		//R3F.buildDroids(1);
+		//R4F.buildDroids(1);
+		
+		//R3F.displayAllDroids();
+		System.out.println();
+		//R4F.displayAllDroids();
+		
+		//R3F.getDroidStorage().get(0).getLeftArm().setLeftArmStatus(Status.OFFLINE);
+		//R3F.getDroidStorage().get(0).checkLeftStatus();
+	}
 }
