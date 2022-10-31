1d6271869512105876a469429ab46c2079c8bcce
Martine Ferrency <martine@ferrency.com>
2022-10-31 16:07:20 -0400

    Edited AstromechDroid class

--- Ferrency_HW4/src/ferrency/DROID/AstromechDroid.java
+++ Ferrency_HW4/src/ferrency/DROID/AstromechDroid.java
@@ -1,5 +1,27 @@
 package ferrency.DROID;
+import ferrency.INTERFACES.Droid;
 
-public class AstromechDroid {
-
+public abstract class AstromechDroid implements Droid {
+	String serialNumber;
+	double height;
+	double weight;
+	
+	SensorDome dome;
+	Chasis chasis;
+	LeftArm leftArm;
+	RightArm rightArm;
+	
+	public AstromechDroid(String serialNumber, double height, double weight, SensorDome dome, Chasis chasis, LeftArm leftArm, RightArm rightArm) {
+		this.serialNumber = serialNumber;
+		this.height = height;
+		this.weight = weight;
+		this.dome = dome;
+		this.chasis = chasis;
+		this.leftArm = leftArm;
+		this.rightArm = rightArm;
+	}
+	
+	public abstract void checkStatus() {
+		//
+	}
 }
