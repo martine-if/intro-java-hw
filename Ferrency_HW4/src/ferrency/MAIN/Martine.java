e120d05896a06dc1ee0ce96cb25497897390cf35
Martine Ferrency <martine@ferrency.com>
2022-11-05 18:15:42 -0400

    Edited main method

--- Ferrency_HW4/src/ferrency/MAIN/Martine.java
+++ Ferrency_HW4/src/ferrency/MAIN/Martine.java
@@ -1,19 +1,23 @@
 package ferrency.MAIN;
 
+import ferrency.ENUMS.Status;
+import ferrency.FACTORY.R3Factory;
+import ferrency.FACTORY.R4Factory;
+
 public class Martine {
 	public static void main(String[] args) {
-		//R3Factory = new R3Factory("R3FAC");
-		//R4Factory = new R4Factory("R4FAC");
+		R3Factory R3F = new R3Factory("R3FAC");
+		R4Factory R4F = new R4Factory("R4FAC");
 		
 		System.out.println();
-		//R3F.buildDroids(1);
-		//R4F.buildDroids(1);
+		R3F.buildDroids(1);
+		R4F.buildDroids(1);
 		
-		//R3F.displayAllDroids();
+		R3F.displayAllDroids();
 		System.out.println();
-		//R4F.displayAllDroids();
+		R4F.displayAllDroids();
 		
-		//R3F.getDroidStorage().get(0).getLeftArm().setLeftArmStatus(Status.OFFLINE);
+		R3F.getDroidStorage().get(0).getLeftArm().setLeftArmStatus(Status.OFFLINE);
 		//R3F.getDroidStorage().get(0).checkLeftStatus();
 	}
 }
