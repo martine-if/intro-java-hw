a83626e20966c6373fb689f274d282a80436dba6
Martine Ferrency <martine@ferrency.com>
2022-11-05 16:16:42 -0400

    R3 and R4 edited

--- Ferrency_HW4/src/ferrency/DROID/R3.java
+++ Ferrency_HW4/src/ferrency/DROID/R3.java
@@ -1,5 +1,24 @@
 package ferrency.DROID;
 
-public class R3 {
+import ferrency.ENUMS.Battery;
+import ferrency.ENUMS.Status;
 
+public class R3 extends AstromechDroid{
+	
+	Status R3Status = Status.ONLINE;
+	Battery R3Battery = Battery.R3;
+	
+	public R3(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void checkStatus() {
+		//
+	}
+	
+	public void displayDroid() {
+		//display info 
+	}
+	
 }
--- Ferrency_HW4/src/ferrency/DROID/R4.java
+++ Ferrency_HW4/src/ferrency/DROID/R4.java
@@ -1,5 +1,25 @@
 package ferrency.DROID;
 
-public class R4 {
+import ferrency.ENUMS.Battery;
+import ferrency.ENUMS.Radar;
+import ferrency.ENUMS.Status;
 
+public class R4 extends AstromechDroid{
+	
+	Status R4Status = Status.ONLINE;
+	Battery R4Battery = Battery.R4;
+	Radar R4Radar = Radar.R4R;
+	
+	public R4(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void checkStatus() {
+		//
+	}
+	
+	public void displayDroid() {
+		//display info 
+	}
 }
