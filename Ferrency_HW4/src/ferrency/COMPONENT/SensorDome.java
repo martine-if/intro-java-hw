e0d5212cef7d99ec6e2d00ad3f9925b09e6b2d4e
Martine Ferrency <martine@ferrency.com>
2022-11-05 16:22:33 -0400

    Components edited

--- Ferrency_HW4/src/ferrency/COMPONENT/SensorDome.java
+++ Ferrency_HW4/src/ferrency/COMPONENT/SensorDome.java
@@ -1,5 +1,51 @@
 package ferrency.COMPONENT;
 
+import ferrency.ENUMS.Status;
+
 public class SensorDome {
 
+	private String serialNumber;
+	private double height;
+	private double weight;
+	private Status status = Status.ONLINE;
+	
+	public SensorDome(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public boolean sensorDomeCheck() {
+		return true;
+	}
 }
