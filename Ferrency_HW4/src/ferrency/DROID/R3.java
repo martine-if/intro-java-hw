8b505323f0ff6dc9663efd27631c9644971bb287
Martine Ferrency <martine@ferrency.com>
2022-11-05 17:21:14 -0400

    Edited R3 and R4 methods

--- Ferrency_HW4/src/ferrency/DROID/R3.java
+++ Ferrency_HW4/src/ferrency/DROID/R3.java
@@ -12,13 +12,32 @@ public class R3 extends AstromechDroid{
 		super(serialNumber);
 	}
 
+
 	@Override
 	public void checkStatus() {
-		//
+		if(this.getDome().getStatus() == Status.OFFLINE || this.getChasis().getStatus() == Status.OFFLINE || this.getLeftArm().getLeftArmStatus() == Status.OFFLINE || this.getRightArm().getRightArmStatus() == Status.OFFLINE) {
+			System.out.println("R3 Astromech is OFFLINE");
+			R3Status = Status.OFFLINE;
+		}
+		else{
+			System.out.println("R3 Astromech is ONLINE");
+			R3Status = Status.ONLINE;
+		}
 	}
 	
 	public void displayDroid() {
 		//display info 
+		System.out.println("R3 Astromech");
+		System.out.println("Serial Number: " + this.getSerialNumber());
+		System.out.println("Status: " + R3Status);
+		System.out.println("Battery: " + R3Battery);
+		System.out.println("Sensor Dome Serial Number " + this.getDome().getSerialNumber());
+		System.out.println("Chasis Serial Number " + this.getChasis().getSerialNumber());
+		System.out.println("Left Arm Serial Number " + this.getLeftArm().getSerialNumber());
+		System.out.println("Right Arm Serial Number " + this.getRightArm().getSerialNumber());
+		System.out.println();
+		checkStatus();
+		
 	}
 	
 }
--- Ferrency_HW4/src/ferrency/DROID/R4.java
+++ Ferrency_HW4/src/ferrency/DROID/R4.java
@@ -16,10 +16,27 @@ public class R4 extends AstromechDroid{
 
 	@Override
 	public void checkStatus() {
-		//
+		if(this.getDome().getStatus() == Status.OFFLINE || this.getChasis().getStatus() == Status.OFFLINE || this.getLeftArm().getLeftArmStatus() == Status.OFFLINE || this.getRightArm().getRightArmStatus() == Status.OFFLINE) {
+			System.out.println("R3 Astromech is OFFLINE");
+			R4Status = Status.OFFLINE;
+		}
+		else{
+			System.out.println("R3 Astromech is ONLINE");
+			R4Status = Status.ONLINE;
+		}
 	}
 	
 	public void displayDroid() {
 		//display info 
+		System.out.println("R3 Astromech");
+		System.out.println("Serial Number: " + this.getSerialNumber());
+		System.out.println("Status: " + R4Status);
+		System.out.println("Battery: " + R4Battery);
+		System.out.println("Sensor Dome Serial Number " + this.getDome().getSerialNumber());
+		System.out.println("Chasis Serial Number " + this.getChasis().getSerialNumber());
+		System.out.println("Left Arm Serial Number " + this.getLeftArm().getSerialNumber());
+		System.out.println("Right Arm Serial Number " + this.getRightArm().getSerialNumber());
+		System.out.println();
+		checkStatus();
 	}
 }
